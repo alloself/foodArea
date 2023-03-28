6f2790bb95e77dcbc425d37c57c81b5c3daaf7e7
Алексей Цапаев <a.tsapaev@think24.ru>
2023-03-29 00:21:28 +0300

    234

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -14,4 +14,10 @@ class MyWebhookHandler extends WebhookHandler
 			Log::debug(123);
       $this->chat->html("Welcome {$member->firstName()}")->send();
 	}
+
+	public function start()
+	{
+			$userFirstName = $this->message->from()->firstName();
+			$this->chat->message("Welcome $userFirstName")->send();
+	}
 }
