5a2fd2a9c2881cb5dd3c8e3bb7823fb8248c7c2c
Alexey <48336899+alloself@users.noreply.github.com>
2023-03-28 22:25:30 +0300

    init

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -3,12 +3,28 @@
 
 namespace App\Http\Webhooks;
 
+use DefStudio\Telegraph\DTO\User;
 use \DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
 
 class MyWebhookHandler extends WebhookHandler
 {
     public function hi(string $userName)
     {
         $this->chat->markdown("*Hi* $userName, happy to be here!")->send();
+    }
+
+    protected function handleUnknownCommand(Stringable $text): void
+    {
+        if (!self::$handleUnknownCommands) {
+            parent::handleUnknownCommand($text);
+        }
+
+        $this->chat->html("I can't understand your command: $text")->send();
+    }
+
+    protected function handleChatMemberJoined(User $member): void
+    {
+        $this->chat->html("Welcome {$member->firstName()}")->send();
     }
 }
