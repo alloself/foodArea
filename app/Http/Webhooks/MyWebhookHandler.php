5fc4f87f51b680d43affa7884cc488944c9387cf
Алексей Цапаев <a.tsapaev@think24.ru>
2023-03-29 19:27:31 +0300

    htrhrt

--- app/Http/Webhooks/MyWebhookHandler.php
+++ app/Http/Webhooks/MyWebhookHandler.php
@@ -4,18 +4,25 @@
 namespace App\Http\Webhooks;
 
 use \DefStudio\Telegraph\Handlers\WebhookHandler;
-use DefStudio\Telegraph\Keyboard\Button;
+use DefStudio\Telegraph\Keyboard\ReplyButton;
+use DefStudio\Telegraph\Keyboard\ReplyKeyboard;
 use DefStudio\Telegraph\Keyboard\Keyboard;
 
 class MyWebhookHandler extends WebhookHandler
 {
 	public function start()
 	{
-		$this->chat->message("Главное меню")->replyKeyboard(function (Keyboard $keyboard) {
-			return $keyboard
-				->button('foo')->requestPoll()
-				->button('bar')->requestQuiz()
-				->button('baz')->webApp('https://webapp.dev');
-		})->send();
+
+		$keyboard = ReplyKeyboard::make()
+			->row([
+				ReplyButton::make('Send Contact')->requestContact(),
+				ReplyButton::make('Send Location')->requestLocation(),
+			])
+			->row([
+				ReplyButton::make('Quiz')->requestQuiz(),
+			]);
+
+		$this->chat->message('hello world')
+			->replyKeyboard($keyboard)->send();
 	}
 }
